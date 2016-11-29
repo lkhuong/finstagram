02f397d251f0905e6a4e17a31690edeaef006330
Lilianne Khuong <lilianne.khuong@gmail.com>
2016-11-29 03:11:16 +0000

    Added sessions

--- app/actions.rb
+++ app/actions.rb
@@ -1,7 +1,12 @@
+helpers do
+    def current_user
+        User.find_by(id: session[:user_id])
+    end
+end
+
 get '/' do
     @posts = Post.order(created_at: :desc)
-    erb :index
-    
+    erb(:index)
 end
 
 get '/signup' do
@@ -13,14 +18,38 @@ post '/signup' do
     email = params[:email]
     avatar_url = params[:avatar_url]
     username = params[:username]
-    password = params[password]
+    password = params[:password]
     
     @user = User.new({ email: email, avatar_url: avatar_url, username: username, password: password })
         
     if @user.save
-        "User #{username} saved!"
+        redirect to('/login')
     else
         erb(:signup)
     end
     
+end
+
+get '/login' do
+    erb(:login)
+end
+
+post '/login' do
+    username = params[:username]
+    password = params[:password]
+    
+    user = User.find_by(username: username)
+    
+    if user && user.password == password
+        session[:user_id] = user.id
+        redirect to('/')
+    else
+       @error_message = "Login failed."
+       erb(:login)
+    end
+end
+
+get '/logout' do
+    session[:user_id] = nil
+    redirect to('/')
 end
